8f23633f2e24f0e576113a367684d0f00d7835ad
Ganesh Tiwari <Ganesht049@gmail.com>
2022-01-21 18:19:56 +0530

    created a hook for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -5,66 +5,20 @@ import "./App.css";
 import { ethers } from "ethers";
 import axios from "axios";
 import config from "./config";
+import Navbar from "./Components/Navbar";
+import useAuthentication from "./Hooks/useAuthentication";
 
 function App() {
-  let provider: ethers.providers.Web3Provider,
-    signer: ethers.providers.JsonRpcSigner;
-
-  if ((window as any).ethereum) {
-    if (!(window as any).ethereum?.enabled) {
-      (window as any).ethereum.enable();
-    }
-    provider = new ethers.providers.Web3Provider((window as any).ethereum);
-
-    signer = provider.getSigner();
-  }
-
-  const getChallenge = async () => {
-    // const address = await signer.signMessage("Hello World");
-    const address = await signer.getAddress();
-
-    const response = await axios.post(
-      config.API_BASE + `login/challenge/${address}`
-    );
-
-    const { id, challenge } = response.data;
-
-    const signedData = await signer.signMessage(challenge);
-
-    const challengeResponse = {
-      challenge: {
-        id,
-        challenge,
-      },
-      signature: signedData,
-      address,
-    };
-
-    const authData = await axios.post(
-      config.API_BASE + `login/response`,
-      challengeResponse
-    );
-
-    const { expires, token } = authData?.data;
-
-    localStorage.setItem("expires", expires);
-    localStorage.setItem("token", token);
-  };
-
-  let expires = localStorage.getItem("expires");
-  let token = localStorage.getItem("token");
+  const { authenticate, isAuthenticated } = useAuthentication();
 
   useEffect(() => {
-    if (expires && new Date(expires) > new Date()) {
-      console.log("currently Authenticated");
-    }
-  }, [expires, token]);
-
-  useEffect(() => {})
+    console.log({ isAuthenticated });
+  }, [isAuthenticated]);
 
   return (
     <div className="App">
-      <button onClick={getChallenge}>Login</button>
+      <Navbar />
+      <button onClick={authenticate}>Login</button>
     </div>
   );
 }
---
+++ src/Hooks/useAuthentication.tsx
@@ -0,0 +1,67 @@
+import { ethers } from "ethers";
+import axios from "axios";
+import config from "../config";
+import { useEffect, useState } from "react";
+
+export default function useAuthentication(): {
+  provider: ethers.providers.Web3Provider;
+  signer: ethers.providers.JsonRpcSigner;
+  authenticate: () => void;
+  isAuthenticated: boolean;
+} {
+  if ((window as any).ethereum) {
+    if (!(window as any).ethereum?.enabled) {
+      (window as any).ethereum.enable();
+    }
+  } else {
+    throw "Please use a browser with Metamask";
+  }
+
+  let provider: ethers.providers.Web3Provider =
+    new ethers.providers.Web3Provider((window as any).ethereum);
+  let signer: ethers.providers.JsonRpcSigner = provider.getSigner();
+  let [authenticated, setAuthenticated] = useState<boolean>(false);
+
+  const authenticate = async () => {
+    // const address = await signer.signMessage("Hello World");
+    const address = await signer?.getAddress();
+
+    const response = await axios.post(
+      config.API_BASE + `login/challenge/${address}`
+    );
+
+    const { id, challenge } = response.data;
+
+    const signedData = await signer?.signMessage(challenge);
+
+    const challengeResponse = {
+      challenge: {
+        id,
+        challenge,
+      },
+      signature: signedData,
+      address,
+    };
+
+    const authData = await axios.post(
+      config.API_BASE + `login/response`,
+      challengeResponse
+    );
+
+    const { expires, token } = authData?.data;
+
+    localStorage.setItem("expires", expires);
+    localStorage.setItem("token", token);
+  };
+
+  useEffect(() => {
+    const expires = localStorage.getItem("expires");
+    const token = localStorage.getItem("token");
+
+    if (expires && new Date(expires) > new Date()) {
+      setAuthenticated(true);
+    }
+  });
+
+  return { provider, signer, authenticate, isAuthenticated: authenticated };
+}
